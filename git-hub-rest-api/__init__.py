d0770d2dcbfb280aff30faf02308d01b3395eb4b
jihun90 <jihun90.kim@icloud.com>
2023-06-12 22:07:58 +0900

    Acess Tocken Api 생성

--- git-hub-rest-api/__init__.py
+++ git-hub-rest-api/__init__.py
@@ -1,11 +1,40 @@
-from flask import Flask
+from flask import Flask, request, make_response, jsonify
+from flask_cors import CORS
+import os
+import requests
+from urllib.parse import urlencode
 
 
 def create_app():
     app = Flask(__name__)
+    cors = CORS(app, resources={r"*": {"origins": "*"}}, supports_credentials=True)
 
     @app.route("/")
     def hello_pybo():
         return "Hello, Pybo!"
+
+    @app.route("/login", methods=["GET", "POST"])
+    def login():
+        if request.method == "POST":
+            # client_id = request.get_data("cleint_id")
+            # client_secrets = request.get_data("client_secrets")
+            headers = {"Accept": "application/json"}
+            query_string = urlencode(request.get_json())
+            github_oauth_url = f"{os.environ.get('GITHUB_OAUTH_URL')}?{query_string}"
+
+            response = requests.post(github_oauth_url, headers=headers)
+            token_json = response.json()
+            error = token_json.get("error", None)
+
+            if error is not None:
+                raise Exception("Error")
+
+            return token_json
+
+        return "Error"
+
+    def build_actual_response(response):
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
 
     return app
